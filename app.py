1fdc33101f092f0554fdf7ae51bec8ed2b700df3
Tahar-M <103082080+Tahar-M@users.noreply.github.com>
2022-04-24 23:34:53 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+import plotly.express as px
+import pdf2image
+
+import extract_data_ocr
+
+
+data = pd.read_csv('data.csv')
+data['date'] = data['date'].apply( lambda date : datetime.strptime(date,"%d/%m/%Y") )
+labels= {'1230':'Fournisseur 1', '1231': 'Fournisseur 2','1232': 'Fournisseur 3'}
+
+data['fournisseur']= data['N_tva'].apply( lambda x : labels[str(x)])
+
+data = data.sort_values(by=["date"],ascending=True )
+
+
+data= data.set_index('date')
+
+cl1, cl2 = st.columns(2)
+
+with cl1:
+    start_date = st.date_input('From',value = min(data.index), min_value= min(data.index),  max_value=max(data.index))
+    st.success(f'Start date from :  {start_date}')
+
+with cl2:
+    end_date = st.date_input('Until',value = max(data.index), min_value= start_date,  max_value=max(data.index))
+    st.success(f'until  :  {end_date}')
+
+
+data = data.loc[start_date:end_date]
+
+data2 = data.copy()
+
+data2 = data2.reset_index()
+
+
+
+
+
+
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Nombre de fournisseurs", f"{len(set(data.N_tva))}")
+col2.metric("Nombre de Factures", f"{len(set(data.N_facture))}")
+col3.metric("Total ", f"{round(data.TTC.sum(),2)} €")
+
+
+fig = px.pie(data, values='TTC', names='fournisseur', color='N_tva',hole=0.5)
+
+fig.update_layout(
+    title={
+        'text': "Repartion de depenses par Fournisseurs ",
+        'x':0.45,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+
+st.plotly_chart(fig, use_container_width=True)
+
+data2 = data2.sort_values(by=["date"],ascending=True )
+
+data2['year'] = [t.year for t in data2.date]
+data2['month'] = [t.month for t in data2.date]
+data2 = data2.groupby(['year','month','fournisseur']).TTC.apply(sum).reset_index()
+data2["period"] = data2["year"].astype(str) +'-'+ data2["month"].astype(str)
+
+
+fig2 = px.bar(data2, x='period', y="TTC",color='fournisseur',labels={
+                     "TTC": "TTC en €",
+                     "period": "Date par mois",
+                     "fournisseur": "Fournisseur"
+                 } )
+fig2.layout.xaxis.tickvals = pd.date_range(str(start_date), str(end_date), freq='MS')
+fig2.layout.xaxis.tickformat = '%b-%y'
+fig2.update_layout(
+    title={
+        'text': "Depenses par mois ",
+        'x':0.45,
+        'xanchor': 'center',
+        'yanchor': 'top'})
+st.plotly_chart(fig2, use_container_width=True)
+
+
+with st.expander("Data Sheet"):
+
+    st.dataframe(data)
+
+    csv = data.to_csv().encode('utf-8')
+
+    
+
+    st.download_button(
+        label="Export data as CSV",
+        data=csv,
+        file_name='data_sheet.csv',
+        mime='text/csv',
+         )
+
+
+
+imagem_referencia = st.file_uploader("Choose a pdf", type=["pdf"],accept_multiple_files=False)
+
+if imagem_referencia is not None:
+
+    agree = st.checkbox('Add to database')
+
+button = st.button("Confirm")
+
+
+if button and imagem_referencia is not None:
+    with st.spinner('Please Wait'):
+    
+
+        try:
+
+            if imagem_referencia.type == "application/pdf":
+                images = pdf2image.convert_from_bytes(imagem_referencia.read(),800)
+                for page in images:
+                    page.save('recent_upload.jpg', 'JPEG')
+
+                a = extract_data_ocr.Extract_data_OCR('recent_upload.jpg')
+                r = a.get_data()
+                st.write('Extracted results')
+                st.json(r)
+                
+                if agree:
+                    df = pd.read_csv('data.csv')
+                    r_dictionary = pd.DataFrame([r])
+                    output = pd.concat([df, r_dictionary], ignore_index=True)
+                    output.to_csv('data.csv',index=False)
+
+                    st.success('Data extracted and saved properly')
+
+
+                else:
+                    st.success('Data extracted properly')
+
+
+                
+
+
+        except:
+            st.error('Invoice Unkown, Please contact Konta.tech for better service ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
---
+++ extract_data_ocr.py
@@ -0,0 +1,148 @@
+import pytesseract
+from pytesseract import Output
+import cv2
+import re
+import json
+
+class Extract_data_OCR:
+
+    def __init__(self,img_path):
+        
+        self.img_path = img_path
+
+        self.img = cv2.imread(self.img_path)
+      
+        self.d = pytesseract.image_to_data(self.img, output_type=Output.DICT)
+
+        self.d['text'] = [s.lower() for s in self.d['text']]
+
+        self.n_boxes = len(self.d['text'])
+
+
+        self.list_index = [ i for i in range(self.n_boxes) if self.d['text'][i] != '']
+
+        self.done_index = []
+
+        self.lines_by_coordinates = {}
+
+        self.g_centre = []
+
+        self.lines = []
+
+        self.ID = None
+
+        self.p = '[\d]+[.,\d]+|[\d]*[.][\d]+|[\d]+'
+
+        with open('layout.json','r') as  j:
+            self.layout = json.load(j)
+
+        self.whole_string = None
+
+        self.data_dic = {}
+
+
+    def extract_amount_fac(self,string):
+
+        
+
+        return float(re.findall(self.p,string)[0].replace(',',''))
+
+
+    def extract_n_tva(self,string):
+
+    
+
+        s = string[string.find('numero tva'):string.find('numero tva')+18]
+
+        return re.findall(self.p,s)[0].replace(',','')
+
+
+
+    def get_lines(self):
+
+        for k in self.list_index:
+
+            if k not in self.done_index:
+
+                L = [k]
+
+                self.done_index.append(k)
+
+                for j in self.list_index:
+
+                    if j not in self.done_index:
+
+                        if abs((self.d["top"][k]+ (self.d['height'][k]/2)) - ((self.d["top"][j]+ (self.d['height'][j]/2))))<=self.d['height'][k]/4:
+
+                            L.append(j)
+                            self.done_index.append(j)
+                self.lines.append(L)
+                self.g_centre.append(self.d["top"][k]+ (self.d['height'][k]/2))
+
+    def extract_data(self):
+
+        
+
+
+
+        self.data_dic = {self.g_centre[i]: ' '.join([self.d['text'][i] for i in line]) for i,line in enumerate(self.lines) }
+
+        self.whole_string = ' '.join(self.data_dic.values())
+        
+
+        self.ID = self.extract_n_tva(self.whole_string)
+
+        self.data_dic = {u:self.data_dic[v] for u,v in self.layout[self.ID].items()}
+
+
+
+    def get_data(self):
+
+        self.get_lines()
+        self.extract_data()
+
+
+        if self.ID=='1230':
+
+            r = {'date': '/'.join(re.findall(self.p,self.data_dic['date'])),
+            'N_tva': re.findall(self.p,self.data_dic['N_tva'])[0],
+            'N_facture': (re.findall(self.p,self.data_dic['N_facture'].replace('/',''))[0]),
+            'HT': self.extract_amount_fac(self.data_dic['HT']),
+            'Taxe': round(self.extract_amount_fac(self.data_dic['HT'])*0.2,2),
+            'TVA': '20.00%',
+            'TTC': self.extract_amount_fac(self.data_dic['TTC'].replace('/','7'))}
+
+            return r
+
+        elif self.ID=='1231':
+            r = {'date': '/'.join(re.findall(self.p,self.data_dic['date'])),
+            'N_tva': re.findall(self.p,self.data_dic['N_tva'])[0],
+            'N_facture': re.findall(self.p,self.data_dic['N_facture'])[0],
+            'HT': self.extract_amount_fac(self.data_dic['HT']),
+            'Taxe': round(self.extract_amount_fac(self.data_dic['HT'])*0.2,2),
+            'TVA': '20.00%',
+            'TTC': self.extract_amount_fac(self.data_dic['TTC'])}
+
+            return r
+
+        elif self.ID == '1232':
+            r = {'date': '/'.join(re.findall(self.p,self.data_dic['date'])[1:]),
+            'N_tva': re.findall(self.p,self.data_dic['N_tva'])[0],
+            'N_facture': (re.findall(self.p,self.data_dic['N_facture'].replace('/',''))[0]),
+            'HT': self.extract_amount_fac(self.data_dic['HT']),
+            'Taxe': round(self.extract_amount_fac(self.data_dic['HT'])*0.2,2),
+            'TVA': '20.00%',
+            'TTC': self.extract_amount_fac(self.data_dic['TTC'])}
+
+            return r 
+        else:
+            
+            print('Facture inconnue')
+
+
+
+
+    
+
+
+
